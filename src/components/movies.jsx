9a3b4d0264dd6e3d5956b02dce27e4fe7e42171f
Dren-9843 <flnj824@gmail.com>
2021-01-13 23:16:37 -0800

    Working

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -50,14 +50,8 @@ class Movies extends Component {
      this.setState({ sortColumn })
     }
 
-    render() { 
-
-        const { length: count } = this.state.movies;
+    getPagedData = () => {
         const { pageSize, currentPage, sortColumn, selectedGenre, movies: allMovies } = this.state;
-
-        if (count === 0) 
-            return <p>There are no movies in the database</p>;
-
         const filtered = 
         selectedGenre && selectedGenre._id
           ? allMovies.filter(m => m.genre._id === selectedGenre._id) 
@@ -65,10 +59,21 @@ class Movies extends Component {
 
       const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
 
+      const movies = paginate(sorted, currentPage, pageSize);
 
+      return { totalCount: filtered.length, data: movies }
 
-        const movies = paginate(sorted, currentPage, pageSize)
+    }
 
+    render() { 
+
+        const { length: count } = this.state.movies;
+        const { pageSize, currentPage, sortColumn} = this.state;
+
+        if (count === 0) 
+            return <p>There are no movies in the database</p>;
+
+        const {totalCount, data: movies} = this.getPagedData();
 
         return (
     <div className="row">
@@ -80,7 +85,7 @@ class Movies extends Component {
              /> 
         </div> 
         <div className="col">
-        <p>Showing {filtered.length} movies in the database</p>
+        <p>Showing {totalCount} movies in the database</p>
         <MoviesTable 
           movies={movies} 
           sortColumn={sortColumn}
@@ -89,7 +94,7 @@ class Movies extends Component {
           onSort={this.handleSort}
           />
             <Pagination 
-            itemsCount={filtered.length} 
+            itemsCount={totalCount} 
             pageSize={pageSize}  
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
