dbf8fd7bd236eb5bfe1502f86520e08441f9c0ab
Dren-9843 <flnj824@gmail.com>
2021-02-16 09:11:34 -0800

    finished

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -24,9 +24,8 @@ class LoginForm extends Form{
     doSubmit = async () => {
         try {
             const { data } = this.state
-            const { data: jwt} = await login(data.username, data.password);
-            localStorage.setItem('token', jwt);
-            this.props.history.replace("/movies");
+            await login(data.username, data.password);
+            window.location = "/"
         } catch (ex) {
             if (ex.response && ex.response.status === 400) {
                 const errors = {...this.state.errors}
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,22 @@
 import { apiUrl } from '../config.json'
 import http from './httpService'
+import { jwtDecode } from 'jwt-decode';
 const apiEndpoint = apiUrl + "/auth"
 
-export function login(email, password) {
-    return http.post(apiEndpoint, { email, password })
+export async function login(email, password) {
+    const { data: jwt } = await http.post(apiEndpoint, { email, password })
+    localStorage.setItem("token", jwt)
+}
+
+export function logout() {
+    localStorage.removeItem("token");
+}
+
+export function getCurrentUser() {
+    try {
+      const jwt = localStorage.getItem("token")
+      return jwtDecode(jwt);
+    } catch (ex) {
+        return null;
+    }
 }
